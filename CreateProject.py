64f39ad526ebd3096247201e9cc53d1e71292ebf
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2022-06-20 15:53:35 -0700

    Update CreateProject.py

--- CreateProject.py
+++ CreateProject.py
@@ -1,7 +1,11 @@
 import json
 
-
-
+def lambda_handler(event, context):
+    print(event)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from local Lambda! test22 update')
+    }
 def validate_project_details(project_details):
     print("In validate_project_details ", project_details)
     if len(project_details) != 3:
