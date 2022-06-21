5f5aaffe8844f1b79f282ea9c014adafc38a67ae
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2022-06-21 10:05:37 -0700

    Add files via upload

---
+++ tests/TestCreateProject.py
@@ -0,0 +1,20 @@
+import unittest
+
+import CreateProject
+from tests.test_constants import error_msg_invalid_pName, fake_project_details_invalid_name, fake_project_details_valid
+
+
+class Testing(unittest.TestCase):
+
+    def test_validate_project_details(self):
+        self.assertTrue(CreateProject.validate_project_details(fake_project_details_valid))
+
+    def test_validate_project_details_validate_name(self):
+        expected = error_msg_invalid_pName
+        with self.assertRaises(Exception) as e:
+            CreateProject.validate_project_details(fake_project_details_invalid_name)
+        self.assertEqual(expected, str(e.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
