a56bb3e4a5dfb48875f85253fa9e9d13b9749507
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2022-06-21 12:49:37 -0700

    Update test_Constants.py

--- tests/test_Constants.py
+++ tests/test_Constants.py
@@ -1,4 +1,4 @@
 fake_project_details_valid = {"name": "fake_name", "description": "fake_desc", "status": "to do"}
 fake_project_details_invalid_name = {"name": "fake_very_very_long_name", "description": "fake_desc", "status": "to do"}
 
-error_msg_invalid_pName = 'Project detail validation failed. Too long Project Name.'
+error_msg_invalid_pName = 'Project details validation failed. Too long Project Name. Fail test'
